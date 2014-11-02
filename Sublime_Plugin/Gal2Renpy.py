c6c197a7268151fe7bb568fbbb9d03c45d71cf53
dtysky <dtysky@outlook.com>
2014-11-02 21:34:11 +0800

    WTF。。。自动填充应该没啥问题了，但是不知道是不是编码问题，待解决。
    Signed-off-by: dtysky <dtysky@outlook.com>

--- Sublime_Plugin/Gal2Renpy.py
+++ Sublime_Plugin/Gal2Renpy.py
@@ -1,7 +1,7 @@
 import sublime, sublime_plugin
 import json,os,pickle,codecs
 import sys
-game_path=json.load(open(os.path.split(__file__)[0]+'/'+'Gal2Renpy.sublime-settings'))['game_gal2renpy_path']
+game_path=json.load(open(os.path.split(__file__)[0]+'/'+'Path.json'))['game_gal2renpy_path']
 sys.path.append(game_path+'Gal2Renpy')
 from Class import *
 from Keyword import *
@@ -96,7 +96,8 @@ def RangeInit():
 		'sound':US.SoundE,
 		'ef':US.EffectSp,
 		'gf':US.Graph,
-		'key':US.KeyWord
+		'key':US.KeyWord,
+		'chrname':US.ChrName
 	}
 
 class Gal2RenpyCompletions(sublime_plugin.EventListener):
@@ -104,7 +105,23 @@ class Gal2RenpyCompletions(sublime_plugin.EventListener):
 		self.ArgsRange=RangeInit()
 
 	def on_query_completions(self, view, prefix, locations):
-		return ['1','2']
+		def GetNowLine():
+			return view.line(view.sel()[0])
+		def GetLineText(line):
+			return view.substr(line)
+		def ToList(ds):
+			tmp=[]
+			for d in sorted(ds):
+				sublime.message_dialog(d)
+				tmp.append(d)
+			return tmp
+		# Only trigger within Gal2Renpy
+		if not view.match_selector(locations[0],"source.Gal2Renpy"):
+			return []
+		if re.match(r'\s*ch\.',GetLineText(GetNowLine())):
+			return (ToList(self.ArgsRange['chrname']),sublime.INHIBIT_EXPLICIT_COMPLETIONS | sublime.INHIBIT_WORD_COMPLETIONS)
+		
+		return []
 
 class Gal2RenpyCommand(sublime_plugin.TextCommand):
 	EditLast=EditInit()
@@ -118,18 +135,13 @@ class Gal2RenpyCommand(sublime_plugin.TextCommand):
 		def GetNowLine():
 			return self.view.line(self.view.sel()[0])
 		def GetLineText(line):
-			return self.view.substr(line)
+			return self.view.substr(line)	
 		def GetNowPoint():
 			return self.view.sel()[0].b
 		def GetPointRC(pt):
 			return self.view.rowcol(pt)
 		def SetPointRC(r,c):
 			return self.view.text_point(r,c)
-		def SetAllFront(s):
-			#for line in self.view.sel():
-			lines = self.view.lines(self.view.sel()[0])
-			for line in lines:
-				self.view.replace(edit, line, s+self.view.substr(line))
 		def SetViewCursor(pt):
 			self.view.sel().clear()
 			self.view.sel().add(sublime.Region(pt))
@@ -143,7 +155,7 @@ class Gal2RenpyCommand(sublime_plugin.TextCommand):
 				ch=lt.split('.')[1]
 				Replace(line,self.CreatInsertCh(ch))
 			else:
-				SetAllFront('\t')	
+				Insert(pt,'\t')	
 		elif lt=='hpc':
 			pass
 		elif lt=='ef':
