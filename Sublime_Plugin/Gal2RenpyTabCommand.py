b163d45b1a3ef948a335712d6a02dc8e0cf2750e
dtysky <dtysky@outlook.com>
2014-11-04 22:37:00 +0800

    智能补全完成。
    Signed-off-by: dtysky <dtysky@outlook.com>

--- Sublime_Plugin/Gal2RenpyTabCommand.py
+++ Sublime_Plugin/Gal2RenpyTabCommand.py
@@ -14,83 +14,81 @@ Py=Pinyin()
 US=User(game_path)
 
 def EditInit():
-	tmp=None
-	if os.path.exists(game_path+'/User/'+'EditLast'):
-		tmp=pickle.load(codecs.open(game_path+'/User/'+'EditLast','r','utf-8'))
-	else:
-		tmp={
-				'sc':[
-						0,(),
-						(('cp','None'),('sc','None'))
-					],
-				'sw':[
-						0,(),
-						(('s','None'),)
-					],
-				'chlast':'',
-				'ch':{},
-				'bg':[
-						0,(('l','None'),('t','None')),
-						(('m','None'),('s','None'),('w','None'))
-					],
-				'cg':[
-						0,(('l','None'),('t','None')),
-						(('m','None'),('s','None'))
-					],
-				'bgm':[
-						0,(),
-						(('m','None'),)
-					],
-				'sound':[
-						0,(),
-						(('m','None'),)
-					],
-				'date':[
-						0,(),
-						(('m','None'),)
-					],
-				'vd':[
-						0,(),
-						(('m','None'),)
-					],
-				'ef':[
-						1,(('e','None'),('args','None')),
-						(('m','None'),)
-					],
-				'gf':[
-						0,(('l','None'),),
-						(('m','None'),)
-					],
-				'key':[
-						0,(('k','None'),),
-						(('m','None'),('n','None'))
-					],
-				'mode':[
-						0,(),
-						(('m','None'),)
-					],
-				'view':[
-						0,(),
-						(('m','None'),)
-					],
-				'chc':[
-						0,(),
-						(('a','None'),('b','None'))
-					],
-				'renpy':[
-						0,(),
-						(('m','None'),)
-					],
-				'test':[
-						0,(),
-						(('m','None'),)
-					]
-			}
-		for ch in US.ChrName:
-			tmp['ch'][ch]=[
-					1,(('l','None'),('t','None')),
-					(('m',ch),('p','None'),('c','None'),('f','None'),('d','None'))
-					]
+	tmp={
+			'sc':[
+					0,(),(),
+					('cp','sc'),{'cp':'None','sc':'None'}
+				],
+			'sw':[
+					0,(),(),
+					('s',),{'s':'None'}
+				],
+			'chrlast':[
+				1,('l','t'),{'l':'None','t':'None'},
+				('m','p','c','f','d'),{'m':'None','p':'None','c':'None','f':'None','d':'None'}
+				],
+			'bg':[
+					0,('l','t'),{'l':'None','t':'None'},
+					('m','s','w'),{'m':'None','s':'None','w':'None'}
+				],
+			'cg':[
+					0,('l','t'),{'l':'None','t':'None'},
+					('m','s'),{'m':'None','s':'None'}
+				],
+			'bgm':[
+					0,(),(),
+					('m',),{'m':'None'}
+				],
+			'sound':[
+					0,(),(),
+					('m',),{'m':'None'}
+				],
+			'date':[
+					0,(),(),
+					('m',),{'m':'None'}
+				],
+			'vd':[
+					0,(),(),
+					('m',),{'m':'None'}
+				],
+			'ef':[
+					1,('e','args'),{'e':'None','args':'None'},
+					('m',),{'m','None'}
+				],
+			'gf':[
+					0,('l'),{'l':'None'},
+					('m',),{'m':'None'}
+				],
+			'key':[
+					0,('k'),{'k':'None'},
+					(('m','None'),('n','None'))
+				],
+			'mode':[
+					0,(),(),
+					('m',),{'m':'None'}
+				],
+			'view':[
+					0,(),(),
+					('m',),{'m':'None'}
+				],
+			'chc':[
+					0,(),(),
+					('a','b'),{'a':'None','b':'None'}
+				],
+			'renpy':[
+					0,(),(),
+					('m',),{'m':'None'}
+				],
+			'test':[
+					0,(),(),
+					('m',),{'m':'None'}
+				]
+		}
+	for ch in US.ChrName:
+		tmp['ch-'+ch]=[
+				1,('l','t'),{'l':'None','t':'None'},
+				('m','p','c','f','d'),{'m':ch,'p':'None','c':'None','f':'None','d':'None'}
+				]
 	return tmp
 
 class Gal2RenpyTabCommand(sublime_plugin.TextCommand):
@@ -122,9 +120,14 @@ class Gal2RenpyTabCommand(sublime_plugin.TextCommand):
 			return {'pair':pair,'tag':pairt.split(':')[0],'attr':pairt.split(':')[1]}
 		#Run
 		pt = GetNowPoint()
+		ptrc=GetPointRC(pt)
+		if ptrc[0]<200:
+			buf=GetLineText(CreatReg(SetPointRC(0,0),pt))
+		else:
+			buf=GetLineText(CreatReg(SetPointRC(ptrc[0]-200,0),pt))
 		line = GetNowLine()
 		lt = GetLineText(line)
-		if not self.Complete(edit,line,lt):
+		if not self.Complete(edit,line,lt,buf):
 			Insert(pt,'\t')
 			return
 		ptrc=GetPointRC(pt)
@@ -137,80 +140,79 @@ class Gal2RenpyTabCommand(sublime_plugin.TextCommand):
 		SetViewSelect(reg)
 
 
-	def Complete(self,edit,reg,lt):
+	def Complete(self,edit,reg,lt,buf):
 		def Replace(reg,s):
 			self.view.replace(edit,reg,s)
+		def SetEditLast(flag,buf,ch=None):
+			flag_tmp=flag
+			if flag=='chrlast':
+				buf=re.findall(r'[\S\s]*<'+'ch'+r'[\S\s]*?>[\S\s]*?</'+'ch'+'>',buf)
+			elif flag=='ch':
+				buf=re.findall(r'[\S\s]*<ch[\S\s]*?>\s*'+'m:'+ch+r'[\S\s]*?</ch>',buf)
+				flag_tmp='ch-'+ch
+			else:
+				buf=re.findall(r'[\S\s]*<'+flag+r'[\S\s]*?>[\S\s]*?</'+flag+'>',buf)
+			if not buf:
+				return
+			tmp={}
+			for b in re.findall(r'[a-z]+:.*?(?=<|>|\n|\s+[a-z]+:)',buf[len(buf)-1]):
+				[tag,attr]=b.split(':')
+				if tag in self.EditLast[flag_tmp][2]:
+					self.EditLast[flag_tmp][2][tag]=attr
+				elif tag in self.EditLast[flag_tmp][4]:
+					self.EditLast[flag_tmp][4][tag]=attr
+			#sublime.message_dialog(str(buf))
 		if lt not in Keywords:
 			#ch.xx.0/ch.xx.1
 			if re.match(r'\s*ch\.\S+\.\d',lt):
 				ch=lt.split('.')[1]
 				self.EditLast['ch'][ch][0]=int(lt.split('.')[2])
+				SetEditLast('ch',buf,ch)
 				Replace(reg,self.CreatInsertCh(ch))
 			#ch.xx
 			elif re.match(r'\s*ch\.\S+',lt):
 				ch=lt.split('.')[1]
+				SetEditLast('ch',buf,ch)
 				Replace(reg,self.CreatInsertCh(ch))
 			#xx.0/xx.1
 			elif re.match(r'\s*\S+\.\d',lt):
-				tag=lt.split('.')[0]
-				self.EditLast[tag][0]=int(lt.split('.')[1])
-				Replace(reg,self.CreatInsertNormal(tag))
+				flag=lt.split('.')[0]
+				self.EditLast[flag][0]=int(lt.split('.')[1])
+				SetEditLast(lt,buf)
+				Replace(reg,self.CreatInsertNormal(flag))
 			else:
 				return False
 		elif lt=='hpc':
 			pass
-		elif lt=='ef':
-			pass
 		elif lt=='ch':
-			Replace(reg,self.CreatInsertCh(self.EditLast['chlast']))
+			SetEditLast('chrlast',buf)
+			Replace(reg,self.CreatInsertNormal('chrlast'))
 		else:
+			SetEditLast(lt,buf)
 			Replace(reg,self.CreatInsertNormal(lt))
 		return True
 
-
-	def CreatInsertNormal(self,lt):
-		def IsEmpty(tup):
-			if len(tup)==0:
-				return True
-			return False
-
-		so ='<' + lt+' '
-		for t in self.EditLast[lt][1]:
-			if not IsEmpty(t):
-				so+=t[0]+':'+t[1]+' '
+	def CreatInsert(self,flag,buf):
+		so ='<' + flag+' '
+		for t in buf[1]:
+			so+=t+':'+buf[2][t]+' '
 		so=so[:-1]+'>'
-		if self.EditLast[lt][0]==1:
+		if buf[0]==1:
 			so+='\n\t'
-		for t in self.EditLast[lt][2]:
-			if not IsEmpty(t):
-				so+=t[0]+':'+t[1]+' '
+		for t in buf[3]:
+			so+=t+':'+buf[4][t]+' '
 		so=so[:-1]
-		if self.EditLast[lt][0]==1:
+		if buf[0]==1:
 			so+='\n'
-		so+='</'+lt+'>'
+		so+='</'+flag+'>'
 		return so
 
+	def CreatInsertNormal(self,lt):
+		buf=self.EditLast[lt]
+		if lt=='chrlast':
+			return self.CreatInsert('ch',buf)
+		return self.CreatInsert(lt,buf)
+
 	def CreatInsertCh(self,ch):
-		def IsEmpty(tup):
-			if len(tup)==0:
-				return True
-			return False
-		if IsEmpty(ch):
-			pass
-		else:
-			so ='<ch '
-			for t in self.EditLast['ch'][ch][1]:
-				if not IsEmpty(t):
-					so+=t[0]+':'+t[1]+' '
-			so=so[:-1]+'>'
-			if self.EditLast['ch'][ch][0]==1:
-				so+='\n\t'
-			for t in self.EditLast['ch'][ch][2]:
-				if not IsEmpty(t):
-					so+=t[0]+':'+t[1]+' '
-			so=so[:-1]
-			if self.EditLast['ch'][ch][0]==1:
-				so+='\n'
-			so+='</ch>'
-			return so
-		return 'This ch does not exits or init !'
+		buf=self.EditLast['ch-'+ch]
+		return self.CreatInsert('ch',buf)
